855095ebaf63b4f881db077f63aa32e45fdf76f0
Choi Yusun <tanks2438@outlook.kr>
2024-10-08 11:33:27 +0000

    Study: solve chap3's exercise10 homework
    Signed-off-by: Choi Yusun <tanks2438@outlook.kr>

---
+++ chap3/exercise10/Trapezoid.cpp
@@ -0,0 +1,55 @@
+#include "Trapezoid.h"
+
+// general programming method
+template <typename F, typename T>
+auto trapezoid(F f, const T& a, const T& b)
+{
+	int n = 1000;
+	T h = (b - a) / n;
+	
+	T sig = 0;
+	for(int i = 1; i < n; i++) 
+		sig += f(a + (i * h));
+
+	return (h / 2 * (f(a) + f(b))) + (h * sig);
+};
+
+// inheritance method
+double trapezoidInherit(functor_base const& f, double a, double b)
+{
+	int n = 1000;
+	double h = (b - a) / n;
+	
+	double sig = 0;
+	for(int i = 1; i < n; i++) 
+		sig += f(a + (i * h));
+
+	return (h / 2 * (f(a) + f(b))) + (h * sig);
+};
+
+int main()
+{
+	std::cout << "using general programming method" << std::endl;
+	std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "If x in [0, 4], integrate f=exp(3x) using trapezoid rule: " << trapezoid(exp3t{}, 0.0, 4.0) << std::endl;
+	std::cout << "If x in [0, 4], integrate (if x < 1, f=sin(x)), (if x >= 1, f=cos(x)) using trapezoid rule: " << trapezoid(sinonecost{}, 0.0, 4.0) << std::endl;
+	std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "trapezoid(std::sin, 0.0, 2.0): " << "error!" << std::endl;
+	std::cout << "trapezoid((double(*)(double))std::sin, 0.0, 2.0): " << trapezoid((double(*)(double))std::sin, 0.0, 2.0) << std::endl;
+	std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "ext3t{}(4.0) - ext3t{}(0.0): " << exp3t{}(4.0) - exp3t{}(0.0) << std::endl;
+	std::cout << "trapezoid(findiff{exp3t{}, 0.001}, 0.0, 4.0): " << trapezoid(findiff{exp3t{}, 0.001}, 0.0, 4.0) << std::endl;
+
+	std::cout << "\nusing inheritance method" << std::endl;
+	std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "If x in [0, 4], integrate f=exp(3x) using trapezoid rule: " << trapezoidInherit(exp3tInherit{}, 0.0, 4.0) << std::endl;
+	std::cout << "If x in [0, 4], integrate (if x < 1, f=sin(x)), (if x >= 1, f=cos(x)) using trapezoid rule: " << trapezoidInherit(sinonecostInherit{}, 0.0, 4.0) << std::endl;
+	std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "trapezoidInherit(std::sin, 0.0, 2.0): " << "error!" << std::endl;
+	std::cout << "trapezoidInherit((double(*)(double))std::sin, 0.0, 2.0): " << "error!" << std::endl;
+	std::cout << "-----------------------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "exp3tInherit{}(4.0) - exp3tInherit{}(0.0): " << exp3tInherit{}(4.0) - exp3tInherit{}(0.0) << std::endl;
+	std::cout << "trapezoidInherit(findiffInherit{exp3tInherit{}, 0.001}, 0.0, 4.0): " << trapezoidInherit(findiffInherit{exp3tInherit{}, 0.001}, 0.0, 4.0) << std::endl;
+	
+	return 0;
+}
---
+++ chap3/exercise10/Trapezoid.h
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cmath>
+
+// general programming method
+struct exp3t
+{
+	template <typename T>
+	auto operator() (T x) const
+	{
+		return std::exp(3.0 * x);
+	}
+};
+
+struct sinonecost
+{
+	template <typename T>
+	auto operator() (T x) const
+	{
+		return (x < 1) ? (std::sin(x)) : (std::cos(x));
+	}
+};
+
+template <typename F, typename T>
+class findiff
+{
+	public:
+		findiff(F f, T h) : f{f}, h{h} {} 
+		
+		auto operator() (T x) const
+		{
+			return (f(x + h) - f(x)) / h;
+		}
+	private:
+		F f;
+		T h;
+};
+
+template <typename F, typename T>
+auto trapezoid(F f, const T& a, const T& b);
+
+// inheritance method
+struct functor_base
+{
+	virtual double operator() (double x) const = 0;
+	virtual ~functor_base() = default;
+};
+
+class exp3tInherit
+	: public functor_base
+{
+	public:
+		virtual double operator() (double x) const override
+		{
+			return std::exp(3.0 * x);
+		}
+};
+
+class sinonecostInherit
+	: public functor_base
+{
+	public:
+		virtual double operator() (double x) const override
+		{
+			return (x < 1) ? (std::sin(x)) : (std::cos(x));
+		}
+};
+
+class findiffInherit
+	: public functor_base
+{
+	public:
+		findiffInherit(functor_base const& f, double h) : f{f}, h{h} {}
+		
+		virtual double operator() (double x) const override
+		{
+			return (f(x + h) - f(x)) / h;
+		}
+	private:
+		functor_base const& f;
+		double h;
+};
+
+double trapezoidInherit(functor_base const& f, double a, double b);
